a7c51e91aa6e0d71317e94ac3f1418a8f328ae91
Miranda Fluharty <miranda@mirandafluharty.com>
2022-03-06 17:54:38 -0700

    keep going with slot things

--- app/src/split/bluetooth/central.c
+++ app/src/split/bluetooth/central.c
@@ -328,6 +328,8 @@ static uint8_t split_central_chrc_discovery_func(struct bt_conn *conn,
 static uint8_t split_central_service_discovery_func(struct bt_conn *conn,
                                                     const struct bt_gatt_attr *attr,
                                                     struct bt_gatt_discover_params *params) {
+    struct peripheral_slot *slot = peripheral_slot_for_conn(conn);
+
     if (!attr) {
         LOG_DBG("Discover complete");
         (void)memset(params, 0, sizeof(*params));
@@ -342,12 +344,12 @@ static uint8_t split_central_service_discovery_func(struct bt_conn *conn,
     }
 
     LOG_DBG("Found split service");
-    discover_params.uuid = NULL;
-    discover_params.func = split_central_chrc_discovery_func;
-    discover_params.start_handle = attr->handle + 1;
-    discover_params.type = BT_GATT_DISCOVER_CHARACTERISTIC;
+    slot->discover_params.uuid = NULL;
+    slot->discover_params.func = split_central_chrc_discovery_func;
+    slot->discover_params.start_handle = attr->handle + 1;
+    slot->discover_params.type = BT_GATT_DISCOVER_CHARACTERISTIC;
 
-    int err = bt_gatt_discover(conn, &discover_params);
+    int err = bt_gatt_discover(conn, &slot->discover_params);
     if (err) {
         LOG_ERR("Failed to start discovering split service characteristics (err %d)", err);
     }
